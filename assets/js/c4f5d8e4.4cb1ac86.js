1de8db6b9788f6dcc19c4404fd739bea40d29e9c
Dynxsty <dynxsty@javadiscord.net>
2021-05-30 11:44:08 +0200

    Add files via upload

---
+++ assets/js/c4f5d8e4.4cb1ac86.js
@@ -0,0 +1,1 @@
+(self.webpackChunkjava_bot_docs=self.webpackChunkjava_bot_docs||[]).push([[195],{2841:function(e,t,a){"use strict";a.r(t);var c=a(7294),n=a(5977),o=a(4996);t.default=function(){return c.createElement(n.l_,{to:(0,o.Z)("/commands/")})}}}]);
